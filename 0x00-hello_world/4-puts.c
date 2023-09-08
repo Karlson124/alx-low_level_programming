8bd4cf633318eefad8909ec895d75c580809d43e
Karlson124 <karlsongoodnews1@gmail.com>
2023-09-08 05:44:29 -0700

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - main entry point
-*return : Always 0 (success)
-*/
+ *main - The main entry point
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - main function
-*return : Always 0 (success)
-*/
+ *main - main function
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
